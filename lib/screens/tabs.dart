684d5171d3b35de23954391c9a26b10671cfb33e
fandipras7 <fandiprasetyopro@gmail.com>
2023-07-30 21:25:02 +0700

    feat(release): Initial release 1.0.0

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
+
+class TabScreen extends StatefulWidget {
+  const TabScreen({super.key});
+
+  @override
+  State<TabScreen> createState() {
+    return _TabScreen();
+  }
+
+}
+
+class _TabScreen extends State<TabScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = const CategoryScreen();
+    var activePageTitle = 'Categories';
+
+    if(_selectedPageIndex == 1) {
+      activePage = const MealsScreen(title: 'Favorites', meals: []);
+      activePageTitle = 'Favorites';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.set_meal), label: 'Categories'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites')
+        ],
+      ),
+    );
+  }
+
+}
